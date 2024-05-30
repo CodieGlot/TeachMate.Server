87bc33546a6be4b0df6ce9c26bab333f1110c37b
puckHong <topluaqwe@gmail.com>
2024-05-31 00:09:37 +0700

    DB UserOTP

--- TeachMate.Domain/DTOs/EmailReciveDto/EmailReciveDto.cs
+++ TeachMate.Domain/DTOs/EmailReciveDto/EmailReciveDto.cs
@@ -11,7 +11,5 @@ namespace TeachMate.Domain
 
         public string GmailReceiveOTP { get; set; }
 
-
-
     }
 }
---
+++ TeachMate.Domain/Models/UserOTP/UserOTP.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachMate.Domain
+{
+    public class UserOTP
+    {
+        [Key]
+        public int id { get; set; }
+        public string OTP { get; set; }
+        public string gmail { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+
+        public DateTime ExpireAt { get; set; } = DateTime.Now.AddMinutes(10);
+
+
+        
+    }
+}
