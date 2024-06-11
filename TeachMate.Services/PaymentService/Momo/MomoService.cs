4e29a6571f7baf4a76b4d73cf80f9b43a88851e1
Codie <ngnhathuy1224@gmail.com>
2024-06-11 17:42:24 +0700

    [TEACHMATE-44] add momo and zalopay

---
+++ TeachMate.Services/PaymentService/Momo/MomoService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Options;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using TeachMate.Domain;
+
+namespace TeachMate.Services;
+public class MomoService : IMomoService
+{
+    private readonly MomoConfig _momoConfig;
+    public MomoService(IOptions<MomoConfig> momoConfig)
+    {
+        _momoConfig = momoConfig.Value;
+    }
+    public async Task<string> MomoTest()
+    {
+        Guid myuuid = Guid.NewGuid();
+        string myuuidAsString = myuuid.ToString();
+
+        string accessKey = _momoConfig.AccessKey;
+        string secretKey = _momoConfig.SecretKey;
+
+        var request = new MomoCollectionLinkRequest
+        {
+            orderInfo = "pay with MoMo",
+            partnerCode = "MOMO",
+            redirectUrl = "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b",
+            ipnUrl = "https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b",
+            amount = 5000,
+            orderId = myuuidAsString,
+            requestId = myuuidAsString,
+            requestType = "payWithMethod",
+            extraData = "",
+            partnerName = "MoMo Payment",
+            storeId = "Test Store",
+            orderGroupId = "",
+            autoCapture = true,
+            lang = "en"
+        };
+
+        var rawSignature = $"accessKey={accessKey}&amount={request.amount}&extraData={request.extraData}&ipnUrl={request.ipnUrl}&orderId={request.orderId}&orderInfo={request.orderInfo}&partnerCode={request.partnerCode}&redirectUrl={request.redirectUrl}&requestId={request.requestId}&requestType={request.requestType}";
+        request.signature = GetSignature(rawSignature, secretKey);
+
+        using (var client = new HttpClient())
+        {
+            StringContent httpContent = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+            var quickPayResponse = await client.PostAsync("https://test-payment.momo.vn/v2/gateway/api/create", httpContent);
+            var contents = await quickPayResponse.Content.ReadAsStringAsync();
+
+            Console.WriteLine(contents);
+            return contents;
+        }
+    }
+
+    private static string GetSignature(string text, string key)
+    {
+        var encoding = new ASCIIEncoding();
+        var textBytes = encoding.GetBytes(text);
+        var keyBytes = encoding.GetBytes(key);
+        byte[] hashBytes;
+
+        using (var hash = new HMACSHA256(keyBytes))
+            hashBytes = hash.ComputeHash(textBytes);
+
+        return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+    }
+}
---
+++ TeachMate.Services/PaymentService/ZaloPay/ZaloPayService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text;
+using TeachMate.Domain;
+
+public class ZaloPayService : IZaloPayService
+{
+    private readonly ZaloPayConfig _zaloPayConfig;
+    public ZaloPayService(IOptions<ZaloPayConfig> zaloPayConfig)
+    {
+        _zaloPayConfig = zaloPayConfig.Value;
+    }
+    public async Task<string> TestZaloPay()
+    {
+        Random rnd = new Random();
+        var embed_data = new { };
+        var items = new[] { new { itemid = "knb", itemname = "kim nguyen bao", itemprice = 198400, itemquantity = 1 } };
+        var app_trans_id = rnd.Next(1000000); // Generate a random order's ID.
+
+        var param = new Dictionary<string, string>
+        {
+            { "app_id", _zaloPayConfig.AppId },
+            { "app_user", "user123" },
+            { "app_time", GetUnixTimestamp().ToString() },
+            { "amount", "50000" },
+            { "app_trans_id", DateTime.Now.ToString("yyMMdd") + "_" + app_trans_id }, // the trading code must be in the format yymmdd_xxxx
+            { "embed_data", JsonConvert.SerializeObject(embed_data) },
+            { "item", JsonConvert.SerializeObject(items) },
+            { "description", "Lazada - Thanh toán đơn hàng #" + app_trans_id },
+            { "bank_code", "zalopayapp" }
+        };
+
+        var data = _zaloPayConfig.AppId + "|" + param["app_trans_id"] + "|" + param["app_user"] + "|" + param["amount"] + "|"
+            + param["app_time"] + "|" + param["embed_data"] + "|" + param["item"];
+        param.Add("mac", ComputeHmacSha256(_zaloPayConfig.Key1, data));
+
+        var result = await PostFormAsync(_zaloPayConfig.CreateOrderUrl, param);
+
+        foreach (var entry in result)
+        {
+            Console.WriteLine("{0} = {1}", entry.Key, entry.Value);
+        }
+
+        return result.ToString();
+    }
+
+    private long GetUnixTimestamp()
+    {
+        return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+    }
+
+    private string ComputeHmacSha256(string key, string data)
+    {
+        using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(key)))
+        {
+            var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+        }
+    }
+
+    private async Task<Dictionary<string, string>> PostFormAsync(string url, Dictionary<string, string> parameters)
+    {
+        using (var client = new HttpClient())
+        {
+            var content = new FormUrlEncodedContent(parameters);
+            var response = await client.PostAsync(url, content);
+            var responseString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(responseString) ?? new Dictionary<string, string>();
+        }
+    }
+}
