48b9cb1d16d16e55c1dedcbf5f6fdd566f9baf5d
taiduongg <git.taidv1211@gmail.com>
2024-06-24 12:20:54 +0700

    [TEACHMATE-66]Student give feedback and tutor reply

---
+++ TeachMate.Domain/Models/Feedback/TutorReplyFeedback.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachMate.Domain
+{
+    public class TutorReplyFeedback
+    {
+        public int Id { get; set; }
+        public string ReplyContent { get; set; }
+        public DateTime ReplyDate { get; set; }
+        public Guid ReplierId { get; set; }
+        public AppUser Replier { get; set; }
+        public int LearningModuleFeedbackId { get; set; }
+
+        // Navigation property to link to LearningModuleFeedback if needed
+        public LearningModuleFeedback LearningModuleFeedback { get; set; }
+    }
+}
---
+++ TeachMate.Services/Migrations/20240623082012_TutorReplyFeedbacks.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachMate.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class TutorReplyFeedbacks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Response",
+                table: "LearningModuleFeedbacks");
+
+            migrationBuilder.CreateTable(
+                name: "TutorReplyFeedback",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReplyContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReplyDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReplierId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LearningModuleFeedbackId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TutorReplyFeedback", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TutorReplyFeedback_AppUsers_ReplierId",
+                        column: x => x.ReplierId,
+                        principalTable: "AppUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TutorReplyFeedback_LearningModuleFeedbacks_LearningModuleFeedbackId",
+                        column: x => x.LearningModuleFeedbackId,
+                        principalTable: "LearningModuleFeedbacks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TutorReplyFeedback_LearningModuleFeedbackId",
+                table: "TutorReplyFeedback",
+                column: "LearningModuleFeedbackId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TutorReplyFeedback_ReplierId",
+                table: "TutorReplyFeedback",
+                column: "ReplierId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TutorReplyFeedback");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Response",
+                table: "LearningModuleFeedbacks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
