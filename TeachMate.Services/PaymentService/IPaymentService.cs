340bb296f623f9e8458974371ecbb513068fa2b6
puckHong <topluaqwe@gmail.com>
2024-07-02 23:21:00 +0700

    [TeachMate-80]Pay for class and create payment order

--- TeachMate.Services/PaymentService/IPaymentService.cs
+++ TeachMate.Services/PaymentService/IPaymentService.cs
@@ -7,6 +7,9 @@ public interface IPaymentService
     Task<OrderUrlResponseDto> CreateOrderUrl(double amount, PaymentProviderType type);
 
      Task<LearningModulePaymentOrder> CreatePaymentOrder(CreateOrderPaymentDto dto);
-    Task<ResponseDto> PayForClass(int OrderID);
-
+    Task<ResponseDto> PayForClass(int ModuleID);
+    Task<List<LearningModulePaymentOrder>> GetAllPaymentOrder(Guid LearnerId);
+    Task<List<LearningModulePaymentOrder>> GetAllPaymentOrderByModuleID(int moduleID);
+    Task<LearningModulePaymentOrder> GetAllPaymentOrderUnpaidByModuleIdByLearner(int moduleID, Guid LearnerId);
+    Task<List<LearningModulePaymentOrder>> GetAllPaymentOrderByModuleIdByLearner(int moduleID, Guid LearnerId);
 }
