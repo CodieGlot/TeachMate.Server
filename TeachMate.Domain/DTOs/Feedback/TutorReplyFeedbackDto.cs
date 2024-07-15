e1cebc5d702f252a0e4cd90e2adc92df2111cc63
taiduongg <git.taidv1211@gmail.com>
2024-07-15 13:40:31 +0700

    Fix Feedback may be

--- TeachMate.Domain/DTOs/Feedback/TutorReplyFeedbackDto.cs
+++ TeachMate.Domain/DTOs/Feedback/TutorReplyFeedbackDto.cs
@@ -9,7 +9,6 @@ namespace TeachMate.Domain
     public class TutorReplyFeedbackDto
     {
         public string ReplyContent { get; set; }  
-        public DateTime ReplyDate { get; set; } = DateTime.Now;  // Date and time of the reply
 
         public int LearningModuleFeedbackId { get; set; }
 
