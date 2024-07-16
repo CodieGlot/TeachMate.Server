fb3ba256c3e672adcd808c46f5f9e86abbfd4edc
lpdmy <dieumy430@gmail.com>
2024-07-16 22:09:14 +0700

    Resolve conflict

---
+++ TeachMate.Api/Controllers/ChatBotController.cs
@@ -0,0 +1,27 @@
+﻿using LLMSharp.Google.Palm;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TeachMate.Domain;
+using TeachMate.Domain.DTOs.SearchDto;
+using TeachMate.Services;
+
+namespace TeachMate.Api;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ChatBotController : ControllerBase
+{
+    [HttpPost("answer")]
+    public async Task<IActionResult> Get(ChatGPTDto dto)
+    {
+        GooglePalmClient client = new GooglePalmClient("AIzaSyCiAA1m4u9_SE2pNFP2_VE0GS4_sTJjVdg");
+        // var response = await client.GenerateTextAsync(question);
+        List<PalmChatMessage> messages = new()
+            {
+                 new(dto.Question, "0"),
+            };
+
+        var response = await client.ChatAsync(messages, null, null);
+        return Ok(response.Candidates[0].Content);
+    }
+}
---
+++ TeachMate.Services/Migrations/20240716150809_AddQuestionAndAnswer.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachMate.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuestionAndAnswer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Answers_AnswerId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_AnswerId",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuestionId",
+                table: "Answers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_QuestionId",
+                table: "Answers",
+                column: "QuestionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Answers_Questions_QuestionId",
+                table: "Answers",
+                column: "QuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Answers_Questions_QuestionId",
+                table: "Answers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Answers_QuestionId",
+                table: "Answers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionId",
+                table: "Answers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_AnswerId",
+                table: "Questions",
+                column: "AnswerId",
+                unique: true,
+                filter: "[AnswerId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Answers_AnswerId",
+                table: "Questions",
+                column: "AnswerId",
+                principalTable: "Answers",
+                principalColumn: "Id");
+        }
+    }
+}
